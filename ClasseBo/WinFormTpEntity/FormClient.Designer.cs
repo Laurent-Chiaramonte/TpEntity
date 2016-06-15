dfe70edc511c86ef02b6df4104fef5e664912a4b
Laurent-Chiaramonte <chiaramonte.laurent@gmail.com>
2016-06-15 12:08:03 +0200

    dgv

--- ClasseBo/WinFormTpEntity/FormClient.Designer.cs
+++ ClasseBo/WinFormTpEntity/FormClient.Designer.cs
@@ -31,15 +31,22 @@
             this.components = new System.ComponentModel.Container();
             this.label1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
-            this.lstcli = new System.Windows.Forms.ListBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.clientBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.numclientDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nomclientDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.adresseclientDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.cpclientDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.villeclientDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.telclientDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.clientBindingSource)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(54, 31);
+            this.label1.Location = new System.Drawing.Point(469, 12);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(92, 13);
             this.label1.TabIndex = 0;
@@ -49,38 +56,82 @@
             // 
             this.button1.Image = global::WinFormTpEntity.Properties.Resources.Zoom_Loupe;
             this.button1.ImageAlign = System.Drawing.ContentAlignment.BottomRight;
-            this.button1.Location = new System.Drawing.Point(212, 12);
+            this.button1.Location = new System.Drawing.Point(603, 12);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(60, 50);
             this.button1.TabIndex = 1;
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // lstcli
+            // dataGridView1
             // 
-            this.lstcli.DataSource = this.clientBindingSource;
-            this.lstcli.DisplayMember = "nom_client";
-            this.lstcli.FormattingEnabled = true;
-            this.lstcli.Location = new System.Drawing.Point(11, 90);
-            this.lstcli.Name = "lstcli";
-            this.lstcli.Size = new System.Drawing.Size(261, 95);
-            this.lstcli.TabIndex = 2;
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.numclientDataGridViewTextBoxColumn,
+            this.nomclientDataGridViewTextBoxColumn,
+            this.adresseclientDataGridViewTextBoxColumn,
+            this.cpclientDataGridViewTextBoxColumn,
+            this.villeclientDataGridViewTextBoxColumn,
+            this.telclientDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.clientBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(21, 100);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(642, 150);
+            this.dataGridView1.TabIndex = 2;
             // 
             // clientBindingSource
             // 
             this.clientBindingSource.DataSource = typeof(Client);
             // 
+            // numclientDataGridViewTextBoxColumn
+            // 
+            this.numclientDataGridViewTextBoxColumn.DataPropertyName = "num_client";
+            this.numclientDataGridViewTextBoxColumn.HeaderText = "num_client";
+            this.numclientDataGridViewTextBoxColumn.Name = "numclientDataGridViewTextBoxColumn";
+            // 
+            // nomclientDataGridViewTextBoxColumn
+            // 
+            this.nomclientDataGridViewTextBoxColumn.DataPropertyName = "nom_client";
+            this.nomclientDataGridViewTextBoxColumn.HeaderText = "nom_client";
+            this.nomclientDataGridViewTextBoxColumn.Name = "nomclientDataGridViewTextBoxColumn";
+            // 
+            // adresseclientDataGridViewTextBoxColumn
+            // 
+            this.adresseclientDataGridViewTextBoxColumn.DataPropertyName = "adresse_client";
+            this.adresseclientDataGridViewTextBoxColumn.HeaderText = "adresse_client";
+            this.adresseclientDataGridViewTextBoxColumn.Name = "adresseclientDataGridViewTextBoxColumn";
+            // 
+            // cpclientDataGridViewTextBoxColumn
+            // 
+            this.cpclientDataGridViewTextBoxColumn.DataPropertyName = "cp_client";
+            this.cpclientDataGridViewTextBoxColumn.HeaderText = "cp_client";
+            this.cpclientDataGridViewTextBoxColumn.Name = "cpclientDataGridViewTextBoxColumn";
+            // 
+            // villeclientDataGridViewTextBoxColumn
+            // 
+            this.villeclientDataGridViewTextBoxColumn.DataPropertyName = "ville_client";
+            this.villeclientDataGridViewTextBoxColumn.HeaderText = "ville_client";
+            this.villeclientDataGridViewTextBoxColumn.Name = "villeclientDataGridViewTextBoxColumn";
+            // 
+            // telclientDataGridViewTextBoxColumn
+            // 
+            this.telclientDataGridViewTextBoxColumn.DataPropertyName = "tel_client";
+            this.telclientDataGridViewTextBoxColumn.HeaderText = "tel_client";
+            this.telclientDataGridViewTextBoxColumn.Name = "telclientDataGridViewTextBoxColumn";
+            // 
             // FormClient
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
-            this.Controls.Add(this.lstcli);
+            this.ClientSize = new System.Drawing.Size(682, 262);
+            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.Name = "FormClient";
             this.Text = "Clients";
             this.Load += new System.EventHandler(this.FormClient_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.clientBindingSource)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -91,7 +142,13 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.ListBox lstcli;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn numclientDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nomclientDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn adresseclientDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn cpclientDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn villeclientDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn telclientDataGridViewTextBoxColumn;
         private System.Windows.Forms.BindingSource clientBindingSource;
     }
 }
